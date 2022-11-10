4aed41c8a9f566b9b38aeb54314a4c577677c616
SevenErol <lorenzo.maggi1998@gmail.com>
2022-11-10 14:43:24 +0100

    printed on screen navbar and imported img

---
+++ src/components/SiteHeader.vue
@@ -0,0 +1,76 @@
+<script>
+
+export default {
+    name: "SiteHeader",
+    data() {
+        return {
+            navMenu: [
+                {
+                    text: "characters",
+                    href: "#"
+                },
+                {
+                    text: "comics",
+                    href: "#"
+                },
+                {
+                    text: "movies",
+                    href: "#"
+                },
+                {
+                    text: "tv",
+                    href: "#"
+                },
+                {
+                    text: "games",
+                    href: "#"
+                },
+                {
+                    text: "collectibles",
+                    href: "#"
+                },
+                {
+                    text: "videos",
+                    href: "#"
+                },
+                {
+                    text: "fans",
+                    href: "#"
+                },
+                {
+                    text: "news",
+                    href: "#"
+                },
+                {
+                    text: "shop",
+                    href: "#"
+                }
+            ]
+        }
+    }
+}
+
+</script>
+
+
+
+<template>
+
+    <header id="site-header">
+        <div class="logo">
+            <img src="../assets/img/dc-logo.png" alt="">
+        </div>
+        <nav class="navbar">
+            <a :href="item.href" v-for="item in navMenu">
+                {{ item.text }}
+            </a>
+        </nav>
+    </header>
+
+</template>
+
+
+
+<style>
+
+</style>
